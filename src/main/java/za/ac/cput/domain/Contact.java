d1ea1efbb5267a34ef05f68ced671e0cca83cdf8
Mpumzi <mphumzimbula@gmail.com>
2024-03-03 23:31:42 +0200

    Created ContactPojo and test class and Student Pojo and test class.Also updated POM file to include jupiter api and engine dependencies

--- src/main/java/za/ac/cput/domain/Contact.java
+++ src/main/java/za/ac/cput/domain/Contact.java
@@ -1,4 +1,40 @@
 package za.ac.cput.domain;
 
+/**
+ * Mpumzi Mbula
+ * 219053324
+ * 03-03-2024
+ */
 public class Contact {
+    private String phoneNumber;
+    private String email;
+
+    public Contact() {
+    }
+
+    public Contact(String phoneNumber, String email) {
+        this.phoneNumber = phoneNumber;
+        this.email = email;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public String toString() {
+        return "phoneNumber=" + phoneNumber + ", email=" + email + '}';
+    }
 }
